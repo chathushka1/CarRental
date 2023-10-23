869b185998edce86d8220cef676972d87880126a
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-23 12:52:41 +0530

    WebAppInitializer add

--- CarRental/BackEnd/src/main/java/lk/ijse/carrental/WebAppInitializer.java
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/WebAppInitializer.java
@@ -1,4 +1,21 @@
 package lk.ijse.carrental;
 
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
