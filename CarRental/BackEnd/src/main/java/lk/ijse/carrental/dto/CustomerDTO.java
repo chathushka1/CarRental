3eed93e2605d81017f75a53d64737acd646d83bd
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-23 12:27:25 +0530

    customer dto

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/dto/CustomerDTO.java
@@ -0,0 +1,30 @@
+package lk.ijse.carrental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 12:25
+ * 2023-10-23
+ * FrontEnd
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String cID;
+    private String name;
+    private String address;
+    private String eMail;
+    private String contact;
+    private String nic;
+    private String licenseNumber;
+    private String nicPhoto;
+    private String licensePhoto;
+}
