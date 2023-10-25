a2dd8e66d5bd8e9609782ba3ba74240aa25aab82
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-25 22:40:03 +0530

    customer controller Back end

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package lk.ijse.carrental.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 22:38
+ * 2023-10-25
+ * FrontEnd
+ */
+
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin
+public class CustomerController {
+    @Autowired
+
+}
