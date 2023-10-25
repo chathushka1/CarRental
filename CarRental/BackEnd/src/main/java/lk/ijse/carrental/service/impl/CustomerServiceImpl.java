c602c0fb6c0af6090300228f1891865640411ad4
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-25 23:02:17 +0530

    customerServiceImpl implement CustomerService

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,40 @@
+package lk.ijse.carrental.service.impl;
+
+import lk.ijse.carrental.dto.CustomerDTO;
+import lk.ijse.carrental.service.CustomerService;
+
+import java.util.List;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 22:57
+ * 2023-10-25
+ * FrontEnd
+ */
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void addCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO findCustomer(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO c) {
+
+    }
+}
