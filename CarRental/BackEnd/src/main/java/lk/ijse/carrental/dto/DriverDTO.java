730114adfff15d91120f64768848965b12154636
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-27 08:18:38 +0530

    Driver DTO

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/dto/DriverDTO.java
@@ -0,0 +1,28 @@
+package lk.ijse.carrental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 08:17
+ * 2023-10-27
+ * FrontEnd
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class DriverDTO {
+    private String dId;
+    private String dName;
+    private String dAddress;
+    private String dContact;
+    private String dEmail;
+    private String dLicenseNumber;
+    private String dLicensePhoto;
+}
