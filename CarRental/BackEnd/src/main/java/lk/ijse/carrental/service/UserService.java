ac6eb31c1dddf57a91dd4c83eae3d4a596dc37f9
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-29 22:15:45 +0530

    user Service Update

--- CarRental/BackEnd/src/main/java/lk/ijse/carrental/service/UserService.java
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/service/UserService.java
@@ -1,5 +1,9 @@
 package lk.ijse.carrental.service;
 
+import lk.ijse.carrental.dto.UserDTO;
+
+import java.util.List;
+
 /**
  * `@authority` Chathushka Madumal
  * hp
@@ -8,4 +12,19 @@ package lk.ijse.carrental.service;
  * FrontEnd
  */
 public interface UserService {
+    void saveUser(UserDTO usersDTO);
+
+    void updateUser(UserDTO usersDTO);
+
+    void deleteUser(String id);
+
+    UserDTO searchUser(String id);
+
+    List<UserDTO> getAllUsers();
+
+    String generateUserIds();
+
+    UserDTO findByPasswordAndUsername(String password, String username);
+
+    UserDTO findByUsername(String username);
 }
---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package lk.ijse.carrental.service.impl;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 22:15
+ * 2023-10-29
+ * FrontEnd
+ */
+public class UserServiceImpl {
+}
