a6c19da8815d1c6b40903ab8c5fe0104a1509110
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-31 19:25:39 +0530

    update car controller

--- CarRental/BackEnd/src/main/java/lk/ijse/carrental/controller/CarController.java
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/controller/CarController.java
@@ -25,13 +25,13 @@ public class CarController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil saveCar(@RequestBody CarDTO dto){
-        carService.addCar(dto);
+        carService.saveCar(dto);
         return new ResponseUtil("Ok", "Successfully Registered.",null);
     }
 
     @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil searchCar(@PathVariable String id){
-        return new ResponseUtil("Ok", "Successfully Searched.",carService.findCar(id));
+        return new ResponseUtil("Ok", "Successfully Searched.",carService.searchCar(id));
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -47,6 +47,6 @@ public class CarController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil getAllCar(){
-        return new ResponseUtil("Ok", "Successfully Searched.",carService.getAllCar());
+        return new ResponseUtil("Ok", "Successfully Searched.",carService.getAllCars());
     }
 }
