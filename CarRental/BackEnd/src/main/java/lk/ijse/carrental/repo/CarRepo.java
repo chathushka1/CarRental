23c520c61d9bbb905a7278dd8aa6452dd01c5dd4
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-31 19:01:54 +0530

    update car Repo

--- CarRental/BackEnd/src/main/java/lk/ijse/carrental/repo/CarRepo.java
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/repo/CarRepo.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 /**
  * `@authority` Chathushka Madumal
  * hp
@@ -14,18 +16,73 @@ import org.springframework.data.repository.query.Param;
  * FrontEnd
  */
 public interface CarRepo extends JpaRepository<Car, String> {
-    @Query(value = "SELECT vId FROM Car ORDER BY vId Desc LIMIT 1",nativeQuery = true)
-    String generateVId();
+    @Query(value = "SELECT carId FROM Car ORDER BY carId DESC LIMIT 1", nativeQuery = true)
+    String generateCarId();
 
-    @Query(value = "SELECT COUNT(*) FROM Car",nativeQuery = true)
-    int registeredVehicleCount();
+    @Query(value = "SELECT registrationNo FROM Car WHERE registrationNo=:reg ", nativeQuery = true)
+    String searchRegNumberIsExists(@Param("reg") String reg);
 
-    @Query(value = "SELECT * FROM Car WHERE registeredDate=:date",nativeQuery = true)
-    int dailyRegisteredVehicleCount(@Param("date") String date);
+    @Query(value = "UPDATE Car SET availableOrNot=:available WHERE carId=:carId", nativeQuery = true)
+    void carAvailableOrNot(@Param("available") String available, @Param("carId") String carId);
 
-    @Query(value = "SELECT * FROM Car WHERE vId = :id",nativeQuery = true)
-    Customer searchVehicle(@Param("id")String id);
 
-    @Query(value = "SELECT * FROM Car WHERE vRegisterNumber=:vRegisterNumber",nativeQuery = true)
-    Customer findVehicleToReserve(@Param("vRegisterNumber")String vRegisterNumber);
+    /*/Car Details Only/*/
+
+    @Query(value = "SELECT * FROM Car ORDER BY noOfPassengers DESC", nativeQuery = true)
+    List<Car> sortAccordingToPassengersByDescending();
+
+
+    @Query(value = "SELECT * FROM Car ORDER BY noOfPassengers ASC", nativeQuery = true)
+    List<Car> sortAccordingToPassengersByAscending();
+
+
+    @Query(value = "SELECT * FROM Car ORDER BY dailyRatePrice DESC", nativeQuery = true)
+    List<Car> sortAccordingToDailyRatePriceByDescending();
+
+
+    @Query(value = "SELECT * FROM Car ORDER BY dailyRatePrice ASC", nativeQuery = true)
+    List<Car> sortAccordingToDailyRatePriceByAscending();
+
+
+    @Query(value = "SELECT * FROM Car ORDER BY monthlyRatePrice DESC", nativeQuery = true)
+    List<Car> sortAccordingToMonthlyRatePriceByDescending();
+
+
+    @Query(value = "SELECT * FROM Car ORDER BY monthlyRatePrice ASC", nativeQuery = true)
+    List<Car> sortAccordingToMonthlyRatePriceByAscending();
+
+
+    /*/Search Details/*/
+
+    @Query(value = "SELECT * FROM Car WHERE transmissionType LIKE :type% ", nativeQuery = true)
+    List<Car> findByTransmissionType(@Param("type")String type);
+
+
+    @Query(value = "SELECT * FROM Car WHERE brand LIKE :brand% ", nativeQuery = true)
+    List<Car> findByBrand(@Param("brand")String brand);
+
+
+    @Query(value = "SELECT * FROM Car WHERE type LIKE :type% ", nativeQuery = true)
+    List<Car> findByType(@Param("type")String type);
+
+
+    @Query(value = "SELECT * FROM Car WHERE fuelType LIKE :fuelType% ", nativeQuery = true)
+    List<Car> findByFuelType(@Param("fuelType")String fuelType);
+
+
+    @Query(value = "SELECT * FROM Car WHERE colour LIKE :colour% ", nativeQuery = true)
+    List<Car> findByColour(@Param("colour")String colour);
+
+
+
+    /*/Available or not cars Count/*/
+
+    @Query (value = "SELECT COUNT(*) FROM Car WHERE availableOrNot=:availability", nativeQuery = true)
+    int noOfAvailableOrReservedCars(@Param("availability")String availability);
+
+
+    @Query(value = "SELECT COUNT(*) FROM Car WHERE underMaintainOrNot=:maintain", nativeQuery = true)
+    int needMaintenanceOrUnderMaintenanceCars(@Param("maintain")String maintain);
+
 }
+
