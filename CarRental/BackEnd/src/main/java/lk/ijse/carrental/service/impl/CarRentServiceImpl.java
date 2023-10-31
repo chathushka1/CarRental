e6007d4d57bc155a360d7266e9ec0bd4f0595f13
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-31 20:05:13 +0530

    car rent service impl

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/service/impl/CarRentServiceImpl.java
@@ -0,0 +1,86 @@
+package lk.ijse.carrental.service.impl;
+
+import lk.ijse.carrental.dto.CarRentDTO;
+import lk.ijse.carrental.entity.CarRent;
+import lk.ijse.carrental.repo.CarRepo;
+import lk.ijse.carrental.service.CarRentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 20:03
+ * 2023-10-31
+ * FrontEnd
+ */
+public class CarRentServiceImpl implements CarRentService {
+
+    @Autowired
+    CarRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Override
+    public void addCarRent(CarRentDTO dto) {
+
+    }
+
+    @Override
+    public void updateCarRent(CarRentDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCarRent(String rentId) {
+
+    }
+
+    @Override
+    public CarRentDTO searchCarRent(String rentId) {
+        return null;
+    }
+
+    @Override
+    public List<CarRentDTO> getAllCarRents() {
+        return null;
+    }
+
+    @Override
+    public String generateRentId() {
+        return null;
+    }
+
+    @Override
+    public void updateCarRentStatus(String rentId, String status) {
+
+    }
+
+    @Override
+    public List<CarRentDTO> getCarRentsByStatus(String status) {
+        return null;
+    }
+
+    @Override
+    public List<CarRentDTO> getCarRentsByDrivingLicenceNo(String status, String licenceNo) {
+        return null;
+    }
+
+    @Override
+    public int getTodayBookingCount(String today) {
+        return 0;
+    }
+
+    @Override
+    public List<CarRentDTO> getTodayBookings(String today) {
+        return null;
+    }
+
+    @Override
+    public List<CarRentDTO> getCarRentsByCustomerId(String customerId) {
+        return null;
+    }
+}
