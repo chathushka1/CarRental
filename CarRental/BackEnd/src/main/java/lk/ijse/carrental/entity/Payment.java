e0a8e74e0bac8793f07ee5430c9ad5b667252c97
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-10-31 19:35:38 +0530

    create payment entity

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/entity/Payment.java
@@ -0,0 +1,35 @@
+package lk.ijse.carrental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 19:32
+ * 2023-10-31
+ * FrontEnd
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Payment {
+    @Id
+    private String paymentId;
+    private String date;
+    private double amount;
+
+    /*@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "rentID", referencedColumnName = "rentID",nullable = false)
+    private CarRent rental;*/
+
+    @ManyToOne
+    @JoinColumn(name = "customerId",referencedColumnName = "customerId",nullable = false)
+    private Customer customer;
+}
