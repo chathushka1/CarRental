d31ad15f28d1da618e7d36f70a00d57b6239e7b5
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-11-01 23:14:35 +0530

    update car cc

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/dto/CarDTO.java
@@ -0,0 +1,45 @@
+package lk.ijse.carrental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 09:26
+ * 2023-10-29
+ * FrontEnd
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class CarDTO {
+    private String carId;
+    private String registrationNo;
+    private String colour;
+    private String brand;
+    private String type;
+    private String fuelType;
+    private String transmissionType;
+    private int noOfPassengers;
+    private int freeKmForDay;
+    private int freeKmForMonth;
+    private double pricePerExtraKM;
+    private double dailyRatePrice;
+    private double monthlyRatePrice;
+    private int totalDistanceTraveled;
+    private String availableOrNot;
+    private String damageOrNot;
+    private String underMaintainOrNot;
+    private String fontViewImage;
+    private String backViewImage;
+    private String sideViewImage;
+    private String interiorViewImage;
+    private LocalDate carRegisteredDate;
+
+}
