a300bb9216fd39118b3e48c33e42accd9b07512b
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-11-03 00:08:57 +0530

    car rent

---
+++ CarRental/BackEnd/src/main/java/lk/ijse/carrental/service/CarRentService.java
@@ -0,0 +1,38 @@
+package lk.ijse.carrental.service;
+
+import lk.ijse.carrental.dto.CarRentDTO;
+
+import java.util.List;
+
+/**
+ * `@authority` Chathushka Madumal
+ * hp
+ * 00:08
+ * 2023-11-03
+ * FrontEnd
+ */
+public interface CarRentService {
+    void addCarRent(CarRentDTO dto);
+
+    void updateCarRent(CarRentDTO dto);
+
+    void deleteCarRent(String rentId);
+
+    CarRentDTO searchCarRent(String rentId);
+
+    List<CarRentDTO> getAllCarRents();
+
+    String generateRentId();
+
+    void updateCarRentStatus(String rentId, String status);
+
+    List<CarRentDTO> getCarRentsByStatus(String status);
+
+    List<CarRentDTO> getCarRentsByDrivingLicenceNo(String status, String licenceNo);
+
+    int getTodayBookingCount(String today);
+
+    List<CarRentDTO> getTodayBookings(String today);
+
+    List<CarRentDTO> getCarRentsByCustomerId(String customerId);
+}
